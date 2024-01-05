0915aea61812e43c55d469664ca1ff281f1b4a76
Felix <65795830+felixtj@users.noreply.github.com>
2024-01-05 11:40:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ app = FastAPI()
 
 from langchain_community.document_loaders import AsyncChromiumLoader
 
-urls = ["https://www.centrayasa.com"]
-loader = AsyncChromiumLoader(urls)
-docs = loader.load()
-print(docs[0].page_content[0:100])
+#urls = ["https://www.centrayasa.com"]
+#loader = AsyncChromiumLoader(urls)
+#docs = loader.load()
+#print(docs[0].page_content[0:100])
+
 
 @app.get("/")
 async def root():
