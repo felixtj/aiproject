1c25742a061775343b3e0244eb93f5875c880b18
Felix <65795830+felixtj@users.noreply.github.com>
2024-01-05 11:31:44 +0700

    update1

--- main.py
+++ main.py
@@ -2,12 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+print ("masuk")
+
 @app.get("/")
 async def root():
-    from langchain_community.document_loaders import AsyncChromiumLoader
-
-    urls = ["https://www.centrayasa.com"]
-    loader = AsyncChromiumLoader(urls)
-    docs = loader.load()
-    print(docs[0].page_content[0:100])
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI 8!"}
